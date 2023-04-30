52965df6699e0ee3af3ee78cb0cc5b9251c1e989
Joe Zhu <zhuzhouyue2005@outlook.com>
2023-04-30 20:48:07 +0800

    v1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author="JoeZhu",
     author_email="zhuzhouyue2005@outlook.com",
     description="A tool enables you to upload your img to your Lsky Server",
-    py_modules=['lskyup'],
+    packages=find_packages(),
     license="GPL v3",
     install_requires=['Click', 'Requests'],
     entry_points={
