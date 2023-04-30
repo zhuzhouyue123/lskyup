d73a06ebee31d8863cbddd8521b093deab823dc6
Joe Zhu <zhuzhouyue2005@outlook.com>
2023-04-30 23:12:51 +0800

    v1

--- LskyProUploader/main.py
+++ LskyProUploader/main.py
@@ -21,13 +21,30 @@ def upload_img(url, path, headers):  # 利用request模块，使用POST方式上
         results.raise_for_status()
 
 
+def print_info(ctx, param, value):  # param 为选项变量 click.Option
+    if (not value or ctx.resilient_parsing) and param != "":
+        return
+    with open("config.json") as config_file:
+        settings = json.load(config_file)
+    click.echo("Server: "+settings["Url"])
+    click.echo("Token: "+settings["Token"])
+    ctx.exit()
+
+
 @click.group()  # Click命令组
+@click.option("-i", "--info",
+              is_flag=True,
+              callback=print_info,
+              expose_value=False,
+              is_eager=True,
+              help="Show the current token & server")
 def cli():
     click.echo("Thanks to use LskyProUploader!")  # 感谢使用本工具
 
 
 @cli.command()
 def config():  # 设置url和token
+    """Config the api token and server url"""
     user_token = click.prompt("Please enter your own Lsky token")
     server_url = click.prompt("Please enter your Lsky server's url")
     setting(user_token, server_url)
@@ -36,6 +53,7 @@ def config():  # 设置url和token
 @cli.command()
 @click.argument("img", nargs=-1, type=click.Path(exists=True))
 def upload(img):  # 上传图片
+    """Upload the img"""
     with open("config.json") as config_file:
         settings = json.load(config_file)
     server_url = settings["Url"]
