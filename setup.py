6e71a2873600866c7530bd7a52901b98e29bcaeb
Joe Zhu <zhuzhouyue2005@outlook.com>
2023-05-01 14:38:42 +0800

    v0.0.2 增加了上传前压缩功能

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="LskyProUploader",
     author="JoeZhu",
-    version="0.1",
+    version="0.0.2",
     author_email="zhuzhouyue2005@outlook.com",
     description="A tool enables you to upload your img to your Lsky Server",
     packages=find_packages(),
