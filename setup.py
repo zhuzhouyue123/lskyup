55b5602cd4b4872798ce9f4cdbf72e6de305c756
Joe Zhu <zhuzhouyue2005@outlook.com>
2023-05-01 18:05:32 +0800

    v0.1.0 第一个版本初步完工

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt", "r") as f:
 setup(
     name="LskyProUploader",
     author="JoeZhu",
-    version="0.0.2",
+    version="0.1.0",
     author_email="zhuzhouyue2005@outlook.com",
     description="A tool enables you to upload your img to your Lsky Server",
     packages=find_packages(),
